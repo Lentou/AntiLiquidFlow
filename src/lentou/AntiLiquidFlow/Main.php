19265b15dea71d2b27acf9fa6ffc77971d31a932
Lentou <33175224+Lentou@users.noreply.github.com>
2021-06-17 16:50:44 +0800

    Add files via upload

---
+++ src/lentou/AntiLiquidFlow/Main.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace lentou\AntiLiquidFlow;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\block\BlockSpreadEvent;
+use pocketmine\block\Liquid;
+
+class Main extends PluginBase implements Listener {
+	
+	public function onEnable() : void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	
+	/**
+	 * @ignoreCancelled false
+	 * @priority LOWEST
+	 *
+	 * @param BlockSpreadEvent $event
+	 */
+	public function onBlockSpread(BlockSpreadEvent $event) : void {
+		if ($event->isCancelled()) {
+			return;
+		}
+		if ($event->getSource() instanceof Liquid) {
+			$event->setCancelled();
+		}
+	}
+	
+}
